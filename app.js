a045b24904f2ce6ab8f9cc3b31cae807c59f32e0
sac6inarya <sachinkarya333@gmail.com>
2023-08-23 18:42:26 +0530

    serve html file

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const fs = require("fs");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res, next) => {
-  let data = req.method;
-  fs.writeFile("hey.txt", data, "utf-8", (err) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.post("/", (req, res, next) => {
+  fs.writeFile("hey.txt", "data", "utf-8", (err) => {
     console.error(err);
   });
   res.send("Hey");
