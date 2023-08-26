cdefd02ebb0f8a8e88db249ddaacd3ee593ffe1b
sac6inarya <sachinkarya333@gmail.com>
2023-08-27 02:04:54 +0530

    fetch data using ModelX.findAll() method

--- controllers/appointments.js
+++ controllers/appointments.js
@@ -1,7 +1,11 @@
 const Details = require("../models/details");
-// const { rootDir } = require("../utils/rootDir");
-// const path = require("path");
 
 exports.fetchAllAppointments = (req, res, next) => {
-  res.send("Hiiii");
+  Details.findAll()
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 };
--- public/js/appointments.js
+++ public/js/appointments.js
@@ -2,7 +2,7 @@ const fetchAllAppointments = (req, res, next) => {
   fetch("http://localhost:3000/fetchAllAppointments", {
     mode: "no-cors",
   })
-    .then((res) => console.log(res))
+    .then((res) => console.log("Res", res))
     .catch((err) => {
       console.error(err);
     });
